d3b4fa8d106fc24d4159ad1d411477a514fc1e19
ariadnayak <ariadna@it.kfu.ru>
2022-04-13 22:05:24 +0300

    testing first commit

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import telebot
 
 # http://t.me/movies_revenue_prediction_bot
@@ -9,18 +8,25 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
-
-
+    bot.send_message(message.chat.id,
+                     'Привет!')
 
 @bot.message_handler(commands=['help'])
 def cmd_help(message):
+    bot.send_message(message.chat.id,
+                     'Привет! Я помогу тебе разобраться в моей работе. У меня есть следующие команды: \n \n'
+                     '/dataset_info - получить информацию о наборе данных \n'
+                     '/models_info - получить описание моделей')
 
 
+# @bot.message_handler(commands=['dataset_info'])
+# def cmd_dataset_info(message):
+#    pass
 
-@bot.message_handler(commands=['dataset_info'])
-def cmd_dataset_info(message):
 
+# @bot.message_handler(commands=['models_info'])
+# def cmd_models_info(message):
+#    pass
 
-
-@bot.message_handler(commands=['models_info'])
-def cmd_models_info(message):
+if __name__ == '__main__':
+    bot.infinity_polling()
