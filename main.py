8f2d69d06ec5eb45b1323d0ce829e81db5f9eb69
ariadnayak <ariadna@it.kfu.ru>
2022-04-14 00:41:26 +0300

    testing first commit

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import telebot
+import requests
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # http://t.me/movies_revenue_prediction_bot
 
 token = '5216695845:AAFwPhtMXamZYg-nF7HqPplgv4KhJvGeW6k'
 bot = telebot.TeleBot(token)
 
+durl='https://www.dropbox.com/s/m7z8uj88smjjhd4/movies_raw_dataset.csv?dl=0'
+data = pd.read_csv('movies.csv', sep='\t')
 
 @bot.message_handler(commands=['start'])
 def cmd_start(message):
     bot.send_message(message.chat.id,
                      'Привет!')
+
+@bot.message_handler(commands=['stop'])
+def cmd_start(message):
+    bot.send_message(message.chat.id,
+                     'Пока!')
+
 
 @bot.message_handler(commands=['help'])
 def cmd_help(message):
@@ -19,9 +32,10 @@ def cmd_help(message):
                      '/models_info - получить описание моделей')
 
 
-# @bot.message_handler(commands=['dataset_info'])
-# def cmd_dataset_info(message):
-#    pass
+@bot.message_handler(commands=['dataset_info'])
+def cmd_dataset_info(message):
+    bot.send_message(message.chat.id, 'Dataset доступен по ссылке ' + durl + ' и состоит из полей:')
+    bot.send_message(message.chat.id, data[:1])
 
 
 # @bot.message_handler(commands=['models_info'])
